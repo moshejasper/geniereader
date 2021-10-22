8d26ff6a8c5ccb613153bc4b230975ba58b93083
Moshe Jasper <moshe.jasper@unimelb.edu.au>
2021-10-22 16:20:12 +1100

    import documentation

--- R/package-geniereader.R
+++ R/package-geniereader.R
@@ -1,7 +1,8 @@
 #' @importFrom magrittr `%>%`
 #' @importFrom stringr str_split str_sub str_length
 #' @importFrom readr read_lines read_file read_tsv
-#' @importFrom dplyr filter select
+#' @importFrom dplyr filter select mutate
 #' @importFrom tidyr pivot_longer
 #' @importFrom ggplot2 ggplot aes geom_line ggtitle xlab ylab theme_bw scale_color_manual geom_point
+#' @importFrom tibble add_column
 NULL
